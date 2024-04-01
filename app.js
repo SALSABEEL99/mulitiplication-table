92b2d0c7b68275f8293f48aca0a570aafa77ba08
SALSABEEL99 <sabeelbaloch21@gmail.com>
2024-04-01 15:47:32 +0500

    table html & css  complete

---
+++ app.js
@@ -0,0 +1,7 @@
+function getInfo() {
+    let input=document.getElementById("input").value;
+    let output=document.getElementById("output");
+    let clear=document.getElementById("clear");
+    let textarea=document.getElementById("input");
+    
+}
