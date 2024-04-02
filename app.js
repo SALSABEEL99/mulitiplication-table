8ed9aa13f7c09863707f0c7160109992907d5d68
SALSABEEL99 <sabeelbaloch21@gmail.com>
2024-04-02 07:56:07 +0500

    complete table done

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 function getInfo() {
-    let input=document.getElementById("input").value;
-    let output=document.getElementById("output");
-    let clear=document.getElementById("clear");
-    let textarea=document.getElementById("input");
-    
+  var input = document.getElementById("input").value;
+  var output = document.getElementById("output");
+  var clear = document.getElementById("clear");
+  var textarea = document.getElementById("input");
+  if (input <= 0 || input > 1000) {
+    alert("please enter the valid number or the number between 1 and 1000 ");
+  } else {
+    output.innerHTML = " ";
+    output.style.display = "block";
+    for (var i = 1; i <= 10; i++) {
+      var result = `${input} * ${i} = ${input * i} <hr>`;
+      output.innerHTML += result;
+    }
+    textarea.value = " ";
+    clear.style.display = "block";
+  }
+}
+function clearTheTable() {
+  var output = document.getElementById("output");
+  var clear = document.getElementById("clear");
+  clear.style.display = "none";
+  output.style.display = "none";
 }
